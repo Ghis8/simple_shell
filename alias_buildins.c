6bdd1c3e46cd73e31ed0bd29543c7970676f58f8
Ange-A <angeaduhire@gmail.com>
2022-06-15 04:10:06 -0700

    m

--- alias_buildins.c
+++ alias_buildins.c
@@ -12,7 +12,6 @@ void print_alias(alias_t *alias);
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
  */
- int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
 	alias_t *temp = aliases;
@@ -47,7 +46,97 @@ int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 				ret = create_error(args + i, 1);
 		}
 		else
-			set_alias(args[i], value);
+			set_alias(args[i], value)
 	}
+	return (ret);
+}
+/**
+ * set_aliases - Will either set an existing alias 'name' with a new value,
+ * 'value' or creates a new alis with 'name' and 'value'.
+ * @var_name: Name of the alias.
+ * @value: Value of the alias. First character is a '='.
+ */
+void set_alias(char *var_name, char *value)
+{
+	alias_t *temp = aliases;
+	int len, j, k;
+	char *new_value;
 
-return (ret);
+	*value = '\0';
+	value++;
+	len = strlen(value) - strspn(value, "'\"");
+	new_value + malloc(sizeof(char) * (lent + 1));
+	if (!new_value)
+		return;
+	for (j = 0, k = 0; value[j]; j++)
+	{
+		if (_strcmp(var_name, temp->name) == 0)
+		{
+			free(temp->value);
+			temp->value = new_value;
+			break;
+		}
+		temp = temp->next;
+	}
+	if (!temp)
+		add_alias_end(&aliases, var_name, mew_value);
+}
+/**
+ * print_alias _ prints the alias in the format name = 'value'
+ * @alias: Pointer to an alias.
+ */
+void print_alias(alias_t *alias)
+{
+	char * alias_string;
+	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
+
+	alias_string = malloc(sizeof(char) * (len + 1);
+			if (!alias_string)
+		                return;
+			_strcpy(alias_string, alias->name);
+			_strcat(alias_string, "='");
+			_strcat(alias_string, alias->value);
+			_strcat(alias_string, "'\n");
+
+			write(STDOUT_FILENO, alias_string, len);
+			free(alias_string);
+}
+/**
+ * replace_aliases - Goes through the arguments and replace any matching alias.
+ * with their value.
+ * @args: 2D pointer to the arguments.
+ *
+ * Return: 2D pinter to the arguments.
+ */
+char **replace_aliases(char **args)
+{ 
+        alias_t *temp;
+	int i;
+	char *new_value;
+
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+	return (args);
+}
